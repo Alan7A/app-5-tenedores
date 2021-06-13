e20670ad4ebfd40565f00b8ca32720d01259b4b5
Alan Delgado <7a.alan@gmail.com>
2021-06-12 23:43:35 -0500

    Loading component

--- app/screens/Account/Account.js
+++ app/screens/Account/Account.js
@@ -1,10 +1,21 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import Loading from '../../components/Loading'
+import UserGuest from './UserGuest'
+import UserLogged from './UserLogged'
+import * as firebase from 'firebase'
 
 export default function Account() {
+    const [login, setLogin] = useState(null)
+
+    useEffect(() => {
+        firebase.default.auth().onAuthStateChanged((user) => {
+            !user ? setLogin(false) : setLogin(true)
+        })
+    }, [])
+
+    if(login === null) return <Loading isVisible={true} text="Cargando..." />
+
     return (
-        <View>
-            <Text>Account</Text>
-        </View>
+        login ? <UserLogged /> : <UserGuest />
     )
 }
