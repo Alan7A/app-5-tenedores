f210f5001f91334085ff60f993ca7f9717359bc0
Alan Delgado <7a.alan@gmail.com>
2021-06-12 21:11:09 -0500

    Bottom navigation icons

--- app/navigations/BottomNavigation.js
+++ app/navigations/BottomNavigation.js
@@ -6,19 +6,60 @@ import FavoritesStack from './FavoritesStack';
 import TopRestaurantsStack from './TopRestaurantsStack';
 import SearchStack from './SearchStack';
 import AccountStack from './AccountStack';
+import { Icon } from 'react-native-elements';
 
 const Tab = createBottomTabNavigator();
 
 export default function BottomNavigation() {
     return (
         <NavigationContainer>
-            <Tab.Navigator>
-                <Tab.Screen name='Restaurants' component={RestaurantsStack} options={{title:'Restaurantes'}}/>
-                <Tab.Screen name='Favorites' component={FavoritesStack} options={{title:'Favoritos'}}/>
-                <Tab.Screen name='TopRestaurants' component={TopRestaurantsStack} options={{title:'Top 5'}}/>
-                <Tab.Screen name='Search' component={SearchStack} options={{title:'Buscar'}}/>
-                <Tab.Screen name='Account' component={AccountStack} options={{title:'Cuenta'}}/>
+            <Tab.Navigator
+                initialRouteName='Restaurants'
+                tabBarOptions={{
+                    inactiveTintColor: '#646464',
+                    activeTintColor: '#00a680'
+                }}
+                screenOptions={({ route }) => ({
+                    tabBarIcon: ({ color }) => screenOptions(route, color)
+                })}
+            >
+                <Tab.Screen name='Restaurants' component={RestaurantsStack} options={{ title: 'Restaurantes' }} />
+                <Tab.Screen name='Favorites' component={FavoritesStack} options={{ title: 'Favoritos' }} />
+                <Tab.Screen name='TopRestaurants' component={TopRestaurantsStack} options={{ title: 'Top 5' }} />
+                <Tab.Screen name='Search' component={SearchStack} options={{ title: 'Buscar' }} />
+                <Tab.Screen name='Account' component={AccountStack} options={{ title: 'Cuenta' }} />
             </Tab.Navigator>
         </NavigationContainer>
     )
+}
+
+function screenOptions(route, color) {
+    let iconName;
+
+    switch (route.name) {
+        case 'Restaurants':
+            iconName = 'compass-outline'
+            break;
+
+        case 'Favorites':
+            iconName = 'heart-outline'
+            break;
+
+        case 'TopRestaurants':
+            iconName = 'star-outline'
+            break;
+
+        case 'Search':
+            iconName = 'magnify'
+            break;
+
+        case 'Account':
+            iconName = 'home-outline'
+            break;
+
+        default:
+            break;
+    }
+
+    return <Icon type='material-community' name={iconName} size={22} color={color} />
 }
