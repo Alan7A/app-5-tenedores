b15fd76ac99c5ca939e481f4b1420ce64427f398
Alan Delgado <7a.alan@gmail.com>
2021-06-19 21:37:36 -0500

    Agregada imagen principal del restaurante y modal para seleccionar ubicación

--- app/components/Restaurants/AddRestaurantForm.js
+++ app/components/Restaurants/AddRestaurantForm.js
@@ -1,14 +1,21 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { View, Text, StyleSheet, ScrollView, Alert, Dimensions } from 'react-native'
 import { Input, Button, Icon, Avatar, Image } from "react-native-elements";
 import * as MediaLibrary from 'expo-media-library'
 import * as ImagePicker from 'expo-image-picker'
+import * as Location from 'expo-location'
+import MapView, { Marker } from 'react-native-maps'
+import Modal from '../Modal'
+
+const screenWidth = Dimensions.get('window').width
 
 export default function AddRestaurantForm({ toastRef, setIsLoading, navigation }) {
     const [restaurantName, setRestaurantName] = useState('')
     const [restaurantAddress, setRestaurantAddress] = useState('')
     const [restaurantDescription, setRestaurantDescription] = useState('')
     const [imagesSelected, setImagesSelected] = useState([])
+    const [isMapVisible, setIsMapVisible] = useState(false)
+    const [restaurantLocation, setRestaurantLocation] = useState(null)
 
     const addRestaurant = () => {
         console.log(imagesSelected);
@@ -53,6 +60,13 @@ export default function AddRestaurantForm({ toastRef, setIsLoading, navigation }
 
     return (
         <ScrollView style={styles.scrollView}>
+            <View style={styles.viewPhoto}>
+                <Image
+                    source={imagesSelected[0] ? { uri: imagesSelected[0] } : require('../../../assets/img/no-image.png')}
+                    style={{ width: screenWidth, height: 200 }}
+                />
+            </View>
+
             <View style={styles.viewForm}>
                 <Input
                     placeholder='Nombre del restaurante'
@@ -63,6 +77,12 @@ export default function AddRestaurantForm({ toastRef, setIsLoading, navigation }
                     placeholder='Dirección'
                     containerStyle={styles.input}
                     onChange={e => setRestaurantAddress(e.nativeEvent.text)}
+                    rightIcon={{
+                        type: 'material-community',
+                        name: 'google-maps',
+                        color: '#c2c2c2',
+                        onPress: () => setIsMapVisible(true)
+                    }}
                 />
                 <Input
                     placeholder='Descripción del restaurante'
@@ -100,7 +120,78 @@ export default function AddRestaurantForm({ toastRef, setIsLoading, navigation }
                 onPress={addRestaurant}
                 buttonStyle={styles.btnAddRestaurant}
             />
+            <Map
+                isMapVisible={isMapVisible}
+                setIsMapVisible={setIsMapVisible}
+                setRestaurantLocation={setRestaurantLocation}
+                toastRef={toastRef}
+            />
         </ScrollView>
+    )
+}
+
+function Map({ isMapVisible, setIsMapVisible, setRestaurantLocation, toastRef }) {
+    const [location, setLocation] = useState(null)
+
+    useEffect(() => {
+        (async () => {
+            const permission = await Location.requestForegroundPermissionsAsync()
+            if (permission.status !== 'granted') {
+                toastRef.current.show('Tienes que aceptar los permisos de geolocalización para seleccionar la dirección', 4000)
+            } else {
+                const loc = await Location.getCurrentPositionAsync({})
+                setLocation({
+                    latitude: loc.coords.latitude,
+                    longitude: loc.coords.longitude,
+                    latitudeDelta: 0.001,
+                    longitudeDelta: 0.001
+                })
+            }
+        })()
+    }, [])
+
+    const saveLocation = () => {
+        setRestaurantLocation(location)
+        toastRef.current.show('Ubicación guardada correctamente')
+        setIsMapVisible(false)
+    }
+
+    return (
+        <Modal isVisible={isMapVisible} setIsVisible={setIsMapVisible}>
+            <View>
+                {location && (
+                    <MapView
+                        style={styles.mapStyle}
+                        initialRegion={location}
+                        showsUserLocation
+                        onRegionChange={(region) => setLocation(region)}
+                    >
+                        <Marker
+
+                            coordinate={{
+                                latitude: location.latitude,
+                                longitude: location.longitude
+                            }}
+                            draggable
+                        />
+                    </MapView>
+                )}
+                <View style={styles.viewMapBtn}>
+                    <Button
+                        title='Guardar Ubicación'
+                        containerStyle={styles.btnGuardarUbiContainer}
+                        buttonStyle={styles.btnAceptarUbi}
+                        onPress={saveLocation}
+                    />
+                    <Button
+                        title='Cancelar Ubicación'
+                        containerStyle={styles.btnCancelarUbiContainer}
+                        buttonStyle={styles.btnCancelarUbi}
+                        onPress={() => setIsMapVisible(false)}
+                    />
+                </View>
+            </View>
+        </Modal>
     )
 }
 
@@ -145,5 +236,31 @@ const styles = StyleSheet.create({
         width: 70,
         height: 70,
         marginRight: 10
+    },
+    viewPhoto: {
+        alignItems: 'center',
+        height: 200,
+        marginBottom: 20
+    },
+    mapStyle: {
+        width: '100%',
+        height: 550
+    },
+    viewMapBtn: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        marginTop: 10
+    },
+    btnCancelarUbiContainer: {
+        paddingLeft: 5
+    },
+    btnCancelarUbi: {
+        backgroundColor: '#a60d0d'
+    },
+    btnGuardarUbiContainer: {
+        paddingRight: 5
+    },
+    btnGuardarUbi: {
+        backgroundColor: '#00a680'
     }
 })
