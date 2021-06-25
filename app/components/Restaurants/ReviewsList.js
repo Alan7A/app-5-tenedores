9d29a6d3a552351c96ce3bf7e7a15c3a9f95ea6b
Alan Delgado <7a.alan@gmail.com>
2021-06-25 16:22:54 -0500

    Se muestran las reviews de cada restaurante

--- app/components/Restaurants/ReviewsList.js
+++ app/components/Restaurants/ReviewsList.js
@@ -1,16 +1,32 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { Button, Avatar, Rating } from "react-native-elements";
 import { firebaseApp } from "../../utils/firebase";
 import firebase from 'firebase/app'
+import 'firebase/firestore'
 
 const db = firebase.firestore(firebaseApp)
 
-export default function ReviewsList({ navigation, id, setRating }) {
+export default function ReviewsList({ navigation, id }) {
     const [userLogged, setUserLogged] = useState(false)
+    const [reviews, setReviews] = useState([])
 
     firebase.auth().onAuthStateChanged(user => {
         user ? setUserLogged(true) : setUserLogged(false)
+    })
+
+    useEffect(() => {
+        db.collection('reviews')
+            .where('idRestaurant', '==', id).get()
+            .then((response) => {
+                const resultReviews = []
+                response.forEach((doc) => {
+                    const data = doc.data()
+                    data.id = doc.id
+                    resultReviews.push(data)
+                })
+                setReviews(resultReviews)
+            })
     })
 
     return (
@@ -40,10 +56,65 @@ export default function ReviewsList({ navigation, id, setRating }) {
                     </Text>
                 </View>
             )}
+
+            {reviews.map((review, i) => (
+                <ListItem key={i} review={review} />
+            ))}
+        </View>
+    )
+}
+
+function ListItem(props) {
+    const { title, review, rating, createdAt, avatarUser } = props.review
+    const reviewDate = new Date(createdAt.seconds * 1000)
+
+    return (
+        <View style={styles.listItem}>
+            <View style={{ marginRight: 15 }}>
+                <Avatar
+                    size='large'
+                    rounded
+                    containerStyle={{ width: 50, height: 50 }}
+                    source={avatarUser ? { uri: avatarUser } : require('../../../assets/img/avatar-default.jpg')}
+                />
+            </View>
+            <View style={{ flex: 1, alignItems: 'flex-start' }}>
+                <Text style={{ fontWeight: 'bold' }}>{title}</Text>
+                <Text style={styles.review}>{review}</Text>
+                <Rating
+                    imageSize={15}
+                    startingValue={rating}
+                    readonly
+                    type='custom'
+                    tintColor='#f2f2f2'
+                />
+                <Text style={styles.date}>
+                    {reviewDate.getDate()}/{reviewDate.getMonth() + 1}/{reviewDate.getFullYear()}
+                </Text>
+            </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-
+    listItem: {
+        flexDirection: 'row',
+        padding: 10,
+        paddingBottom: 20,
+        borderBottomColor: '#e3e3e3',
+        borderBottomWidth: 1
+    },
+    review: {
+        paddingTop: 2,
+        color: 'grey',
+        marginBottom: 5
+    },
+    date: {
+        marginTop: 5,
+        color: 'grey',
+        fontSize: 12,
+        position: 'absolute',
+        right: 0,
+        bottom: 0
+    }
 })
